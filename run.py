6563ff8151d10567868946eb5e23aa51c963b8b8
demetermarcell <demeter.marcell@gmail.com>
2025-03-17 21:32:47 +0000

    Setup environment, wire up APIs and confirm access to google sheet

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('cfmcred.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('ci_car_fuel_metrics')
+
+honda_fit = SHEET.worksheet('honda_fit')
+
+data = honda_fit.get_all_values()
+
+
+print(data)
