69388a29e63805a12ab0b43af879d6647e86c058
demetermarcell <demeter.marcell@gmail.com>
2025-03-19 09:02:29 +0000

    Amend cred.json file name

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
-CREDS = Credentials.from_service_account_file('cfmcred.json')
+CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('ci_car_fuel_metrics')
